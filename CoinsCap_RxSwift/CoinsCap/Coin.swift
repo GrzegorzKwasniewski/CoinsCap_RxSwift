1e403f532aa61c4302fb7c2317d3538208da6c66
Grzegorz Kwasniewski <contact@grzegorzkwasniewski.com>
2018-05-18 15:07:12 +0200

    ADD Coin Model

---
+++ CoinsCap_RxSwift/CoinsCap/Coin.swift
@@ -0,0 +1,20 @@
+//
+//  Coin.swift
+//  CoinsCap_RxSwift
+//
+//  Created by Grzegorz Kwasniewski on 18/05/2018.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct Coin {
+    
+    private(set) var coinName: String
+    private(set) var coinPrice: String
+    
+    init(coinData: JSON) {
+        coinName = coinData.dictionary?["name"]?.string ?? "no data"
+        coinPrice = coinData.dictionary?["price_usd"]?.string ?? "no data"
+    }
+}
