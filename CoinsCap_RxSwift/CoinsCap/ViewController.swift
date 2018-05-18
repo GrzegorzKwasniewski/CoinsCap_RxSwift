ed12b75b8847a03c2aeafc55e3b1ca79540b47d6
Grzegorz Kwasniewski <contact@grzegorzkwasniewski.com>
2018-05-18 15:14:43 +0200

    ADD Refresh Controll

--- CoinsCap_RxSwift/CoinsCap/ViewController.swift
+++ CoinsCap_RxSwift/CoinsCap/ViewController.swift
@@ -3,23 +3,37 @@
 //  CoinsCap_RxSwift
 //
 //  Created by Grzegorz Kwasniewski on 17/05/2018.
-//  Copyright © 2018 Grzegorz Kwasniewski. All rights reserved.
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UITableViewController {
+    
+    fileprivate let events = Variable<[Coin]>([])
+    fileprivate let bag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        setupRefreshControll()
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupRefreshControll() {
+        
+        self.refreshControl = UIRefreshControl()
+        let refreshControl = self.refreshControl!
+        
+        refreshControl.backgroundColor = UIColor(white: 0.98, alpha: 1.0)
+        refreshControl.tintColor = UIColor.darkGray
+        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        
     }
-
-
+    
+    @objc func refresh() {
+        
+    }
 }
 
