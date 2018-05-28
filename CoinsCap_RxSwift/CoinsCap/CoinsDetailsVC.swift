4b68fb15334c342db6dc1a366a7e55215e1c9af7
Grzegorz Kwasniewski <contact@grzegorzkwasniewski.com>
2018-05-28 16:47:33 +0200

    ADD Basic UI For CoinDetalisVC

--- CoinsCap_RxSwift/CoinsCap/CoinsDetailsVC.swift
+++ CoinsCap_RxSwift/CoinsCap/CoinsDetailsVC.swift
@@ -8,6 +8,16 @@ import SwiftyJSON
 
 class CoinsDetailsVC: UIViewController {
     
+    @IBOutlet weak var coinName: UILabel!
+    @IBOutlet weak var coinValue: UILabel!
+    
     var singleCoin = Coin(coinData: JSON())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        coinName.text = singleCoin.coinName
+        coinValue.text = singleCoin.coinPrice
+    }
     
 }
